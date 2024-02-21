8e1754030eadaf787333c2be68ef75ee4be836ac
Thomas Berlok <tberlok@gmail.com>
2024-02-21 14:45:25 +0100

    Cartesian tiling and smoothing filter

--- turbocluster/cartesian_tiling.py
+++ turbocluster/cartesian_tiling.py
@@ -1,19 +1,12 @@
-import numpy as np
 import cupy as cp
 from numba import cuda
-import numba
 
 
 @cuda.jit(device=True, inline=True)
 def find_tile_single_dim(particle_pos, nx, tile_width):
     """
-    Would be faster to find the half that it is...
     """
-    tile_index = 0
-    pos = tile_width
-    while particle_pos > pos:
-        pos += tile_width
-        tile_index += 1
+    tile_index = particle_pos // tile_width
     return tile_index
 
 
@@ -44,30 +37,58 @@ def get_tile_information(tile_index, particles_per_tile, start_index_for_tile):
     ip = cuda.grid(1)
     if ip < tile_index.shape[0]:
         ip_tile_x, ip_tile_y, ip_tile_z = tile_index[ip, :]
-        cuda.atomic.add(particles_per_tile, (ip_tile_x, ip_tile_y, ip_tile_z), 1)
-        cuda.atomic.min(start_index_for_tile, (ip_tile_x, ip_tile_y, ip_tile_z), ip)
+        cuda.atomic.add(particles_per_tile,
+                        (ip_tile_x, ip_tile_y, ip_tile_z), 1)
+        cuda.atomic.min(start_index_for_tile,
+                        (ip_tile_x, ip_tile_y, ip_tile_z), ip)
+
 
 class CartesianTiling:
     """
     """
 
-    def __init__(self, pos, center, widths,
-                 npix, threadsperblock):
-        
-        npix_x = npix
-        npix_y = int(widths[1] / widths[0] * npix)
-        npix_z = int(widths[2] / widths[0] * npix)
+    def __init__(self, positions, npix=128, threadsperblock=256):
 
+        Np = positions.shape[0]
 
+        blocks_1d = (Np + (threadsperblock - 1)) // threadsperblock
+
+        # Copy positions
+        self._pos = cp.array(positions)
+
+        # Convert self._position to units from [0, 1) * 2**L
+        self.off_sets = cp.min(self._pos, axis=0)
+        self._pos -= self.off_sets[None, :]
+
+        max_pos = cp.max(self._pos, axis=0)
+
+        self.conversion_factor = (npix - 1) / max_pos[0]
+        self.npixs = (max_pos * self.conversion_factor).astype(int) + 1
+
+        npix_x = int(self.npixs[0])
+        npix_y = int(self.npixs[1])
+        npix_z = int(self.npixs[2])
+
+        self.tile_widths = max_pos / self.npixs
+        self.tile_widths_internal = self.tile_widths * self.conversion_factor
+
+        self._pos *= self.conversion_factor
 
         # Get tile information
-        particles_per_tile = cp.zeros((npix_x, npix_y, npix_z), dtype=int)
+        self.tile_index = cp.zeros((Np, 3), dtype=int)
+
+        self.particles_per_tile = cp.zeros((npix_x, npix_y, npix_z), dtype=int)
         # Initialize to value larger than the largest posssible tileindex
-        start_index_for_tile = cp.ones((npix_x, npix_y, npix_z), dtype=int) * pos.shape[0] + 1
-        get_tile_information(tile_index, particles_per_tile, start_index_for_tile)
+        self.start_index_for_tile = cp.ones(
+            (npix_x, npix_y, npix_z), dtype=int) * Np + 1
 
-        # self.sort_index = 
-        # self.tile_index = 
+        find_tile_index[blocks_1d, threadsperblock](
+            self._pos, self.npixs, self.tile_widths_internal, self.tile_index)
 
-    def find_tiles(self):
-        pass
+        get_tile_information[blocks_1d, threadsperblock](
+            self.tile_index, self.particles_per_tile,
+            self.start_index_for_tile)
+
+        self.sort_index = cp.argsort(self.tile_index[:, 2] * npix_x * npix_y
+                                     + self.tile_index[:, 1] * npix_x +
+                                     self.tile_index[:, 0])
--- turbocluster/smoothing_filter.py
+++ turbocluster/smoothing_filter.py
@@ -1,10 +1,9 @@
 import numpy as np
 import cupy as cp
-from numba import cuda
-import numba
-from .. import util
-from .. import settings
-from .. import units
+# from numba import cuda
+# import numba
+import paicos as pa
+from .cartesian_tiling import CartesianTiling
 
 
 class SmoothingFilter:
@@ -12,14 +11,55 @@ class SmoothingFilter:
     """
 
     def __init__(self, snap, center, widths, orientation=None,
-                 max_filter_length=None, npix=128, threadsperblock=8):
+                 max_filter_length=None, npix=128, threadsperblock=256):
 
         if orientation is not None:
             raise RuntimeError('not implemented')
         if max_filter_length is None:
             raise RuntimeError('need input')
 
-        self.max_filter_length = max_filter_length
+        self.snap = snap
+
+        code_length = self.snap.length
+
+        if hasattr(center, 'unit'):
+            self.center = center.copy
+            assert center.unit == code_length.unit, 'this restriction applies'
+        elif pa.settings.use_units:
+            self.center = np.array(center) * code_length
+        else:
+            self.center = np.array(center)
+
+        if hasattr(widths, 'unit'):
+            self.widths = widths.copy
+            assert widths.unit == code_length.unit, 'this restriction applies'
+        elif pa.settings.use_units:
+            self.widths = np.array(widths) * code_length
+        else:
+            self.widths = np.array(widths)
+
+        if hasattr(max_filter_length, 'unit'):
+            self.max_filter_length = max_filter_length.copy
+            assert max_filter_length.unit == code_length.unit, 'this restriction applies'
+        elif pa.settings.use_units:
+            self.max_filter_length = np.array(max_filter_length) * code_length
+        else:
+            self.max_filter_length = np.array(max_filter_length)
+
+        if orientation is None:
+            self.orientation = None
+        else:
+            self.orientation = orientation.copy
+
+        self.pos = self.snap["0_Coordinates"]
+
+        self._do_region_selection()
+
+        # Create tiling
+        self.tile = CartesianTiling(self.pos, npix=npix,
+                                    threadsperblock=threadsperblock)
+
+        self.gpu_variables['pos'] = self.gpu_variables['pos'][self.tile.sort_index, :]
 
     def _do_region_selection(self):
 
@@ -29,15 +69,17 @@ class SmoothingFilter:
 
         # Send subset of snapshot to GPU
         # get the index of the region of projection
+        ones = np.ones(self.snap["0_Coordinates"].shape[0])
+        thickness = self.max_filter_length * ones
         if self.orientation is None:
-            get_index = util.get_index_of_cubic_region_plus_thin_layer
+            get_index = pa.util.get_index_of_cubic_region_plus_thin_layer
             self.index = get_index(self.snap["0_Coordinates"],
-                                   center, widths, self.max_filter_length,
+                                   center, widths, thickness,
                                    snap.box)
         else:
-            get_index = util.get_index_of_rotated_cubic_region_plus_thin_layer
-            self.index = get_index(snap["0_Coordinates"],
-                                   center, widths, self.max_filter_length, snap.box,
+            get_index = pa.util.get_index_of_rotated_cubic_region_plus_thin_layer
+            self.index = get_index(self.snap["0_Coordinates"],
+                                   center, widths, thickness, snap.box,
                                    self.orientation)
 
         self.pos = self.pos[self.index]
@@ -46,7 +88,7 @@ class SmoothingFilter:
 
     def _send_data_to_gpu(self):
         self.gpu_variables = {}
-        if settings.use_units:
+        if pa.settings.use_units:
             self.gpu_variables['pos'] = cp.array(self.pos.value)
         else:
             self.gpu_variables['pos'] = cp.array(self.pos)
@@ -55,7 +97,7 @@ class SmoothingFilter:
             self.gpu_variables['rotation_matrix'] = cp.array(
                 self.orientation.rotation_matrix)
 
-        if settings.use_units:
+        if pa.settings.use_units:
             self.gpu_variables['widths'] = cp.array(self.widths.value)
             self.gpu_variables['center'] = cp.array(self.center.value)
         else:
